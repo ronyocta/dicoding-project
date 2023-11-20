026bcbf280f63bd6c05d69640b29a980c2e9467d
Rony Octa <148183515+ronyocta@users.noreply.github.com>
2023-11-20 15:35:47 +0700

    Add files via upload

--- Dashboard/dashboard.py
+++ Dashboard/dashboard.py
@@ -2,12 +2,10 @@ import streamlit as st
 import plotly.express as px
 import pandas as pd
 import os
-import warnings
-warnings.filterwarnings("ignore")
 
 st.set_page_config(page_title="Bicycle Rental", page_icon=":bar_chart:", layout="wide")
 
-st.title(":bar_chart: Sample Bicycle Rental EDA")
+st.title(":bar_chart: Bicycle Rental (Exploratory Data Analysis)")
 st.markdown("<style>div.block-container{padding-top:1rem;}</style>", unsafe_allow_html=True)
 
 url = "https://raw.githubusercontent.com/ronyocta/dicoding-project/master/Dashboard/main_data.csv"
